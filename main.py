cfc48704c95cfe4ee0110c470e997e64b6debed3
Jack Wolf <mystinker08@gmail.com>
2020-08-30 07:13:02 -0500

    initial base working script

---
+++ main.py
@@ -0,0 +1,23 @@
+"""Following script is to automate my kids school fever check survey that is 
+to be submitted every morning before she can enter school. On an already busy 3 kids
+going to school morning. The survey can only be accessed once a school day."""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+
+browser.set_window_size(600, 600)
+browser.set_window_position(0, 0)
+
+#browser.get("https://www.brookfieldlagrange.iad1.qualtrics.com")
+browser.get("https://brookfieldlagrange.iad1.qualtrics.com/jfe/form/SV_5uoAn5rdUOcUiqx?Q_DL=GQiC2BdRdpm00g6_5uoAn5rdUOcUiqx_CGC_J0q5oiAd598GByx&Q_CHL=email")
+
+#sleep(5)
+
+
+
+
+#sleep(5)
+#browser.close()
